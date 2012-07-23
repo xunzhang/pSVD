1a1b89f8ef3c3301ecb1221e7f85da28b99ee7f3
wuhong_intern <wuhong_intern@algdev.intra.douban.com>
2012-07-24 00:50:35 +0800

    svd_tr.cpp updated

--- src/svd_tr.cpp
+++ src/svd_tr.cpp
@@ -1,13 +1,27 @@
 /* svd_tr.cpp */
 
 #include "svd_tr.hpp"
+#include "mat_csr.hpp"
+#include "mat_container.hpp"
 
-void svd_tr(mat_csr &A,
-	    std::vector< vector<double> > &U,
-	    std::vector<double> &S,
-	    std::vector< vector<double> > &V,
-	    size_t over_sample = 0,
-	    double eps = 1.e-7) {
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+
+int svd_tr(mat_csr &A,
+	    mat_container<double> &U,
+	    vector<double> &S,
+	    mat_container<double> &V,
+	    size_t over_sample,
+	    double eps) {
 	
-	size_t m = U.
+	size_t m = U.dim0(), n = V.dim0();
+	size_t nev = std::max(U.dim1(), std::max(V.dim1(), S.size()));
+	if(nev <= 0) return 0;
+	if(nev + (over_sample ? over_sample : nev) >= std::min(m, n)) {
+		return svd_ge(A, U, S, V);
+	}
+	return svd_tr();
 }
+
--- src/svd_tr.hpp
+++ src/svd_tr.hpp
@@ -4,11 +4,12 @@
 
 #include <vector>
 #include "mat_csr.hpp"
+#include "mat_container.hpp"
 
-void svd_tr(mat_csr &A, 
-            std::vector< vector<double> > &U, 
+int svd_tr(mat_csr &A, 
+            mat_container<double> &U, 
             std::vector<double> &S, 
-	    std::vector< vector<double> > &V, 
+	    mat_container<double> &V, 
 	    size_t over_sample = 0, 
 	    double eps = 1.e-7);
 
